51b850bf39cacb8798737a3d93f7905b4416b61c
zaurmag411@gmail.com <guKGyVmC7o>
2022-11-16 21:10:27 +0300

    Upd: component FormControl

--- src/components/formElements/FormControl.vue
+++ src/components/formElements/FormControl.vue
@@ -14,14 +14,13 @@
         @blur="$emit('blur')"
         @input="$emit('input', $event)"
       >
-      <button
-        class="btn form-control-toggle"
-        type="button"
-        ref="togglePass"
-      >
-        ПП
-      <!-- <app-icon name="eye" />-->
-      </button>
+<!--      <button-->
+<!--        class="btn form-control-toggle"-->
+<!--        type="button"-->
+<!--        ref="togglePass"-->
+<!--      >-->
+<!--       <svg-icon name="eye" />-->
+<!--      </button>-->
     </div>
 
     <input
@@ -34,76 +33,69 @@
       @blur="$emit('blur')"
       @input="$emit('input', $event)"
     >
-    <div class="invalid-feedback d-block fz-12" v-if="error">{{ error }}</div>
+    <div class="invalid-field" v-if="error">{{ error }}</div>
 
     <slot name="append" />
   </div>
 </template>
 
-<script>
-import { ref, onMounted, computed } from 'vue'
+<script setup>
+import { computed } from 'vue'
+// import SvgIcon from '@/components/ui/SvgIcon.vue'
 // import togglePassword from '@/utils/toggle-password'
 
-export default {
-  name: 'FormControl',
-  emits: ['blur', 'input'],
-  props: {
-    label: {
-      type: String,
-      required: false
-    },
-    id: {
-      type: String,
-      required: true
-    },
-    type: {
-      type: String,
-      required: false,
-      default: 'text'
-    },
-    modelValue: {
-      type: Object
-    },
-    error: {
-      type: String,
-      required: false
-    },
-    placeholder: {
-      type: String,
-      required: false
-    },
-    classListInput: {
-      type: String,
-      required: false
-    },
-    classListLabel: {
-      type: String,
-      required: false
-    }
+const emit = defineEmits(['blur', 'input'])
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: false
   },
-  setup (props, { emit }) {
-    const togglePass = ref(null)
-    const model = computed({
-      get () {
-        return props.modelValue
-      },
-      set (value) {
-        emit('update:modelValue', value)
-      }
-    })
-
-    onMounted(() => {
-      if (togglePass.value) {
-        // togglePassword(togglePass.value)
-      }
-    })
-
-    return {
-      togglePass,
-      model
-    }
+  id: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: false,
+    default: 'text'
+  },
+  modelValue: {
+    type: Object
+  },
+  error: {
+    type: String,
+    required: false
+  },
+  placeholder: {
+    type: String,
+    required: false
+  },
+  classListInput: {
+    type: String,
+    required: false
+  },
+  classListLabel: {
+    type: String,
+    required: false
   }
-}
+})
+
+// const togglePass = ref(null)
+const model = computed({
+  get () {
+    return props.modelValue
+  },
+  set (value) {
+    emit('update:modelValue', value)
+  }
+})
+
+// onMounted(() => {
+//   if (togglePass.value) {
+//     togglePassword(togglePass.value)
+//   }
+// })
 </script>
 
 <style lang="scss" scoped>
@@ -145,9 +137,19 @@ export default {
       transform: translateY(-50%);
     }
 
+    &.is-invalid {
+      border-color: #ff6161;
+    }
+
     &:focus {
       outline: none;
       border-color: #5e74ff;
     }
+  }
+
+  .invalid-field {
+    margin-top: 5px;
+    font-size: 12px;
+    color: #ff6161;
   }
 </style>

3f740e4a9d3ae52ab85efe2c1d5689832298daaa
zaurmag411@gmail.com <guKGyVmC7o>
2022-11-16 21:00:04 +0300

    Upd: add icon in AppButton

--- src/components/ui/AppButton.vue
+++ src/components/ui/AppButton.vue
@@ -6,54 +6,67 @@
     @click="$emit('click')"
     v-bind="attrs"
   >
+    <svg-icon v-if="icon?.placement === 'prepend'" :name="icon?.name" :classList="icon.classList" />
     <slot name="default" />
+    <svg-icon v-if="icon?.placement === 'append'" :name="icon?.name" :classList="icon.classList" />
   </button>
 </template>
 
-<script>
-export default {
-  name: 'AppButton',
-  emits: ['click'],
-  props: {
-    id: {
-      type: String,
-      required: false
-    },
-    classList: {
-      type: String,
-      required: false
-    },
-    animate: {
-      type: Object,
-      required: false,
-      default () {
-        return {}
-      }
-    },
-    type: {
-      type: String,
-      required: false,
-      default: 'button'
-    },
-    attrs: {
-      type: Object,
-      required: false,
-      default () {
-        return {}
-      }
-    },
-    attrsWrapper: {
-      type: Object,
-      required: false,
-      default () {
-        return {}
-      }
+<script setup>
+import SvgIcon from '@/components/ui/SvgIcon.vue'
+
+defineEmits(['click'])
+defineProps({
+  id: {
+    type: String,
+    required: false
+  },
+  classList: {
+    type: String,
+    required: false
+  },
+  animate: {
+    type: Object,
+    required: false,
+    default () {
+      return {}
+    }
+  },
+  type: {
+    type: String,
+    required: false,
+    default: 'button'
+  },
+  icon: {
+    type: Object,
+    required: false,
+    default () {
+      return {}
+    }
+  },
+  attrs: {
+    type: Object,
+    required: false,
+    default () {
+      return {}
+    }
+  },
+  attrsWrapper: {
+    type: Object,
+    required: false,
+    default () {
+      return {}
     }
   }
-}
+})
 </script>
 
 <style scoped lang="sass">
   .btn-wrapper
     display: inline-block
+
+  .btn-close-icon
+    color: #fff
+    width: 15px
+    height: 15px
 </style>

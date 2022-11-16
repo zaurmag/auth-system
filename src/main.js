4a591bd0296ff5ae9dbb3b9372823135a6ef6200
zaurmag411@gmail.com <guKGyVmC7o>
2022-11-16 20:59:13 +0300

    Feat: add component SvgIcon

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import 'virtual:svg-icons-register'
 
 import './assets/scss/main.scss'
 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({ // https://github.com/vbenjs/vite-plugin-svg-icons
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/images/sprite')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]'
+    })
+  ],
   server: {
     port: '8080'
   },

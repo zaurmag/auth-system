3fc350afeaea3f7be815469cd212fbf1ae00a0de
zaurmag411@gmail.com <guKGyVmC7o>
2022-11-16 23:11:38 +0300

    Upd: replace header in separate component
    - Add btn logout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="header">
+    <div class="logo">AuthSystem</div>
+    <div class="user">
+      <div class="user__name">DemoUser</div>
+      <app-button classList="btn-primary btn--s30" @click="logout">Logout</app-button>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import AppButton from '@/components/ui/AppButton.vue'
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const logout = () => {
+  authStore.logout()
+  router.push({ name: 'auth', query: { message: 'auth' } })
+}
+</script>
+
+<style scoped lang="scss">
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .logo {
+      font-size: 16px;
+    }
+  }
+
+  .user {
+    display: flex;
+    align-items: center;
+
+    &__name {
+      margin-right: 15px;
+    }
+  }
+</style>
--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -3,7 +3,7 @@
     <aside class="sidebar">Sidebar</aside>
 
     <div class="content content--right">
-      <header class="header">Header</header>
+      <app-header />
 
       <main class="content__body">
         <router-view />
@@ -12,17 +12,8 @@
       <footer class="footer">Footer</footer>
     </div>
   </div>
-
-<!--  <teleport to="body">-->
-<!--    <app-message />-->
-<!--  </teleport>-->
 </template>
 
 <script setup>
-// import AppMessage from '@/components/ui/AppMessage'
-// import { ref } from 'vue'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
-
-<style scoped lang="sass">
-
-</style>

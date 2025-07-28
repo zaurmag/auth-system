457744cbaedbe32699a34660bbf2d01f0bb52ab3
Zaur_Magomedov <zaurmag411@gmail.com>
2025-07-28 14:39:49 +0300

    install new VueJS TS

--- src/utils/error.js
+++
@@ -1,9 +0,0 @@
-const ERROR_CODES = {
-  EMAIL_NOT_FOUND: 'The user with this email was not found',
-  INVALID_PASSWORD: 'Invalid password',
-  auth: 'Please log in'
-}
-
-export function error (code) {
-  return ERROR_CODES[code] ? ERROR_CODES[code] : 'Unknown error!'
-}
